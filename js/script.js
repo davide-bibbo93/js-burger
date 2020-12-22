5c74bfad85168406d5b81bfaccd563142a17038b
Davide <bibbodavide@gmail.com>
2020-12-22 17:41:04 +0100

    modifiche js e css

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // var costanti
 var priceStandard = 50;
 var discount = 20;
+var couponList = ['GUSTOSO', 'DELIZIOSO', 'ESAGERATO'];
+var arrayTotal = 0;
 // ------------------------------------------------------------------------
 
 // stabilire var richiamando dall'html.
@@ -10,39 +12,39 @@ var coupon = document.getElementById('coupon');
 var button = document.getElementsByTagName('button')[0];
 var total = document.getElementById('final-price')
 var counterIngredients = 0; // flag
-var arrayTotal = 0;
+
 // ------------------------------------------------------------------------
 
 // tutto quello che viene generato al click sul pulsante.
 button.addEventListener('click',function() {
   // nome obbligatorio, se input vuota alert!
   if (nameHamburger.value === '') {
-    alert('Scegli il nome per il burger.')
-  }
+    alert('Scegli il nome per il tuo Burger.')
+  } else if (nameHamburger.value !== '') {
+    // l'utente deve selezionare almeno due ingredienti, sennò alert.
+    // Attribuire value agli ingredienti per somma totale.
+    var counterIngredients = 0; // flag
+    var arrayIngredients = [];
 
-  // l'utente deve selezionare almeno due ingredienti, sennò alert.
-  // Attribuire value agli ingredienti per somma totale.
-  var counterIngredients = 0; // flag
-  var arrayIngredients = [];
-
-  for (var i = 0; i < listIngredients.length; i++) {
-    if (listIngredients[i].checked === true) {
-      counterIngredients += 1; // flag aumenta di uno
-      arrayIngredients.push(parseInt(listIngredients[i].value));
+    for (var i = 0; i < listIngredients.length; i++) {
+      if (listIngredients[i].checked === true) {
+        counterIngredients += 1; // flag aumenta di uno
+        arrayIngredients.push(parseInt(listIngredients[i].value));
+      }
     }
-  }
 
-  for (var i = 0; i < arrayIngredients.length; i++) {
-    arrayTotal += arrayIngredients[i];
-  }
+    for (var i = 0; i < arrayIngredients.length; i++) {
+      arrayTotal += arrayIngredients[i];
+    }
 
-  if (counterIngredients < 2) {
-    alert('Scegli almeno 2 ingredienti.');
+    if (counterIngredients < 2) {
+      alert('Scegli almeno 2 ingredienti.');
+    }
+
   }
 
   // sezione coupon : creare array con stringhe.
   // se l'utente inserisce una delle stringhe, avrà sconto 20%
-  var couponList = ['GUSTOSO', 'DELIZIOSO', 'ESAGERATO'];
   var couponNow = false;
 
   for (var i = 0; i < couponList.length; i++) {
